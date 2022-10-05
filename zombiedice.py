a5a6ff36c890b6769a4049e1009d682fbfbeef8d
Pedro Lor Orlando <48284191+plorlando@users.noreply.github.com>
2022-10-05 01:22:41 +0000

    commit

--- zombiedice.py
+++ zombiedice.py
@@ -2,11 +2,9 @@ import random
 
 
 
-# DEFINIÇÃO DE VARIÁVEIS
+# DEFINIÇÃO DE VARIÁVEIS E FUNCOES
 jogadores = []
 data = {}
-
-
 j = 'jogadores'
 
 def add_player(p):
@@ -19,10 +17,20 @@ def add_player(p):
     })
 
 
+# CONFIGURACOES INICIAIS DO JOGO
+print('*********************************')
+print('ZOMBIE DICE')
+print('*********************************\n')
 while j != '':
     j = input('Digite o nome do jogador (deixe em branco para começar a jogar): ')
     if j != '':
         add_player(j)
 
-print(random.choice(jogadores))
+jogador_inicial = random.choice(jogadores)
+pos = jogadores.index(jogador_inicial)
+total_jog = len(jogadores)
+
+print(f'O jogador que vai iniciar o jogo é {jogador_inicial}')
+print(f'A posição do jogador inicial é {pos}')
+print(f'Total de jogadores: {total_jog}')
 print(data)
