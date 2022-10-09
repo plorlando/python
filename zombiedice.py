9e9b438d910f78f56d7bb2a33313596cb39ac867
Pedro Lor Orlando <plorlando@hotmail.com>
2022-10-09 01:42:30 -0300

    commit

--- zombiedice.py
+++ zombiedice.py
@@ -1,66 +1,11 @@
 import random
+import pandas as pd
 
 
-# DEFINIÇÃO DE VARIÁVEIS E FUNCOES
-# 2 a 99 jogadores
-data = {}
+df = pd.DataFrame()
 
-
-jogadores = []
-
-j = 'jogadores'
-dado_verde = ("c", "e", "c"," p", "c", "p")
-dado_amarelo = ("c", "e", "p", "c", "e", "p")
-dado_vermelho = ("e", "p", "e", "c", "e", "p")
-d1 = dado_verde
-d2 = dado_verde
-d3 = dado_verde
-d4 = dado_verde
-d5 = dado_verde
-d6 = dado_verde
-d7 = dado_amarelo
-d8 = dado_amarelo
-d9 = dado_amarelo
-d10 = dado_amarelo
-d11 = dado_vermelho
-d12 = dado_vermelho
-d13 = dado_vermelho
-
-count_players = 1
-
-def add_player(p, c):
-    # jogadores.append(p)
-    data.update({c: {'name': p,
-        "c": 0,
-        "e": 0,
-        "p": 0
-        }
-    })
-
-
-def play():
-    # deve ser sorteados 3 dados por jogada, cada um deles representa uma vitima atacada
-    # fazer um random para determinar qual dado foi sorteado
-    # fazer um segundo random dentro do dado sorteado para sortear a face do dado
-    # somar o ponto sorteado nos pontos do jogador
-    pass
-
-def score():
-    # deve ser registrado o ponto no dict, com o nome da pessoa e qual faze saiu
-    # a cada ponto deve verificar condição de: 1-vitória, 2-derrota, e se nenhuma acontecer se ele quer jogar novamente
-    pass
-
-def check_win():
-    pass
-
-def check_lose():
-    pass
-
-def check_play_again():
-    pass
-
-def check_tie():
-    pass
+players = []
+num_players = 0
 
 # CONFIGURACOES INICIAIS DO JOGO
 print('*********************************')
@@ -68,27 +13,131 @@ print('ZOMBIE DICE')
 print('São necessários pelo menos 2 jogadores para pode jogar Zombie Dice.')
 print('*********************************\n')
 
-while j != '' and count_players <= 99:
-    j = input(f'Digite o nome do jogador numero {count_players} (deixe em branco para começar a jogar): ')
+green = ("C", "E", "C", "P", "C", "P")
+yellow = ("C", "E", "P", "C", "E", "P")
+red = ("E", "p", "E", "C", "E", "P")
+# d1 = green
+# d2 = green
+# d3 = green
+# d4 = green
+# d5 = green
+# d6 = green
+# d7 = yellow
+# d8 = yellow
+# d9 = yellow
+# d10 = yellow
+# d11 = red
+# d12 = red
+# d13 = red
 
-    if j != '':
-        add_player(j, count_players)
+count_players = 0
 
+num_players = int(input('Quantas pessoas irão jogar? '))
+
+for i in range(0, num_players):
+    players.append([input(f'Digite o nome do jogador número {count_players+1}: '), 0, 0, 0])
     count_players += 1
-    print('\n')
 
-total_players = count_players
-
-if total_players >= 2:
+df = pd.DataFrame(players, columns=['PLAYER', 'C', 'E', 'P'])
+print(df)
 
 
-    # initial_player = random.choice(data['player'])
-    print(data[2]['name'])
-    # pos = data[initial_player]['pos']
+# DEFININDO O JOGADOR INICIAL
+iplayer = df.sample(replace=False)
 
-    # print(f'O jogador que vai iniciar o jogo é {initial_player}')
-    # print(f'A posição do jogador inicial é {pos}')
-    # print(f'Total de jogadores: {total_players}')
-    # print(data)
-else:
-    print('O jogo precisa ter no mínimo 2 jogares. Reinicie o jogo.')
+p = iplayer.iloc[0]['PLAYER']
+index = iplayer.index[0]
+print(index)
+
+# ROLANDO OS DADOS
+print(f'Jogador {p}, role os dados')
+
+# df.loc[index, ['C']] = 1
+# df.loc[index, ['E']] = 2
+# df.loc[index, ['P']] = 3
+
+
+# DEFINIÇÃO DE VARIÁVEIS E FUNCOES
+# 2 a 99 jogadores
+
+dice_number = str(random.randrange(1, 14))
+
+dice = 'd' + dice_number
+
+match dice:
+    case 'd1':
+        face = random.sample(green, 1)
+        print(f'd1, {face}')
+    case 'd2':
+        face = random.sample(green, 1)
+        print(f'd2, {face}')
+    case 'd3':
+        face = random.sample(green, 1)
+        print(f'd3, {face}')
+    case 'd4':
+        face = random.sample(green, 1)
+        print(f'd4, {face}')
+    case 'd5':
+        face = random.sample(green, 1)
+        print(f'd5, {face}')
+    case 'd6':
+        face = random.sample(green, 1)
+        print(f'd7, {face}')    
+    case 'd7':
+        face = random.sample(yellow, 1)
+        print(f'd7, {face}')
+    case 'd8':
+        face = random.sample(yellow, 1)
+        print(f'd8, {face}')
+    case 'd9':
+        face = random.sample(yellow, 1)
+        print(f'd9, {face}')
+    case 'd10':
+        face = random.sample(yellow, 1)
+        print(f'd10, {face}')
+    case 'd11':
+        face = random.sample(red, 1)
+        print(f'd11, {face}')
+    case 'd12':
+        face = random.sample(red, 1)
+        print(f'd12, {face}')
+    case 'd13':
+        face = random.sample(red, 1)
+        print(f'd13, {face}')
+    case _:
+        print('default')
+
+
+
+df.loc[index, face] = df.loc[index, face] + 1
+print(df)
+
+
+
+# def play():
+#     # deve ser sorteados 3 dados por jogada, cada um deles representa uma vitima atacada
+#     # fazer um random para determinar qual dado foi sorteado
+#     # fazer um segundo random dentro do dado sorteado para sortear a face do dado
+#     # somar o ponto sorteado nos pontos do jogador
+#     pass
+
+# def score():
+#     # deve ser registrado o ponto no dict, com o nome da pessoa e qual faze saiu
+#     # a cada ponto deve verificar condição de: 1-vitória, 2-derrota, e se nenhuma acontecer se ele quer jogar novamente
+#     pass
+
+# def check_win():
+#     pass
+
+# def check_lose():
+#     pass
+
+# def check_play_again():
+#     pass
+
+# def check_tie():
+#     pass
+
+
+
+
